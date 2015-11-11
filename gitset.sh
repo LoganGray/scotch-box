d4a6895c16672b12bfd4d8b2aad7b4b6fd9ba75c
Logan Gray <logan@intensetech.com>
2015-11-11 16:53:47 -0500

    fix non global setting re: show untracked files

--- gitset.sh
+++ gitset.sh
@@ -7,7 +7,7 @@ git config --global alias.br branch;
 git config --global alias.hist 'log --pretty=format:"%h %ad | %s%d [%an]" --graph --date=short'; 
 git config --global alias.type 'cat-file -t'; 
 git config --global alias.dump 'cat-file -p'; 
-git config status.showuntrackedfiles no; 
+git config --global status.showuntrackedfiles no; 
 git config --global push.default simple;
 
 #GIT setup GIT default user config (customize to yourself)
